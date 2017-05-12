df47435f40952b86c3e26a62f98efd60f93d688b
NguyenTrinhKhanhToan <khanhtoan0896@gmail.com>
2017-05-11 17:22:12 -0700

    TextViewEditTextCheckBoxButtonVaImageButton

---
+++ Bai12TextViewEditTextCheckBoxButtonVaImageButton/gen/com/example/NguyenTrinhKhanhToan_56th2_56132332_Bai12_Java/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.NguyenTrinhKhanhToan_56th2_56132332_Bai12_Java;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
