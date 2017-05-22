2911d265dadaed1688fd95081fa647685e3e0d48
NguyenTrinhKhanhToan <khanhtoan0896@gmail.com>
2017-05-21 23:45:46 -0700

    Lập trình Anndroid ListView nâng cao - custom Adapter

---
+++ LapTrinhAndroidCoBanBai30ListViewNangCao-CustomAdapter-NguyenTrinhKhanhToan/src/com/example/NguyenTrinhKhanhToan/SinhVien.java
@@ -0,0 +1,13 @@
+package com.example.NguyenTrinhKhanhToan;
+
+
+public class SinhVien {
+	public String HoTen;
+	public Integer NamSinh;
+	
+	public SinhVien(String hoTen, Integer namSinh ){
+		HoTen = hoTen;
+		NamSinh = namSinh;
+	}
+
+}
