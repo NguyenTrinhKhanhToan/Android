96f1b037df9a5a877e6c6e36a8953c8edbd87ac3
NguyenTrinhKhanhToan <Madara0896@gmail.com>
2017-06-15 00:34:56 -0700

    Bài 28 xử lý tập tin

---
+++ Bai_28_2_ExternalStorage_56132332/Bai_28_2_ExternalStorage_Nguyen_Trinh_Khanh_Toan_56132332/gen/com/example/bai_28_2_externalstorage_khanh_toan_56132332/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.bai_28_2_externalstorage_khanh_toan_56132332;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Bai_28_3_Savingcachefiles_56132332/Bai_28_3_Savingcachefiles_Nguyen_Trinh_Khanh_Toan_56132332/gen/com/example/bai_28_3_savingcachefiles_khanh_toan_56132332/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.bai_28_3_savingcachefiles_khanh_toan_56132332;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
